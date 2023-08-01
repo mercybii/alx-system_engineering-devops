ca4f32ba36ce44691c51bf2a275c2ccb885eb36d
mercybii <mercybii95@gmail.com>
2023-08-01 21:53:42 +0300

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -5,10 +5,14 @@
 
 regex = /school/
 
+#read an input 
 input = ARGV[0]
 
+#find all occurence of the regex pattern
 matches = input.scan(regax)
 
+#join the matched
 matched_string = matches.join
 
+#output of the matched string
 puts matched_string
