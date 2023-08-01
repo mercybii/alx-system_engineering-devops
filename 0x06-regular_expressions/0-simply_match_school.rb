37712b57c080745e0dd1c155d1a9f31707d9b960
mercybii <mercybii95@gmail.com>
2023-08-01 21:50:11 +0300

    0-simply_match_school.rb

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+#define the regular expression 
+
+
+regex = /school/
+
+input = ARGV[0]
+
+matches = input.scan(regax)
+
+matched_string = matches.join
+
+puts matched_string
